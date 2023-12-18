61ef027742e1205a045c3a60b4b4191c337c9902
Conrad <conrad@hh-dev.com>
2023-12-18 11:34:14 -0600

    Updated readme and put initial examples starting points

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# python-api-wrapper-examples
-Example library showing how to use the HH Data Management Python API Wrapper
+# HH Data Management Python API wrapper examples
+
+This repository serves as library of examples showing how to use the HH Data Management Python API Wrapper.
+
+In all examples an [API key](https://help.hh-dm.com/extensibility/api/#authentication) is needed to authenticate against the API.
+
+## Table of Contents
---
+++ example_runsheet_laps.py
@@ -0,0 +1,24 @@
+import asyncio
+import json
+
+import hhdm_apiclient_wrapper as hh
+
+def main():
+    api_key = 'YOUR_API_KEY'
+
+    auth_settings = hh.AuthenticationSettings(
+        api_key=api_key,
+        authentication_mode=hh.AuthenticationMode.API_KEY,
+    )
+    client = hh.ApiClient(authentication_manager=hh.AuthenticationManager(auth_settings))
+
+    print(f'Api client wrapper v{client.get_version()}. Running lap average computation demo.')
+
+    result = asyncio.run(client.get_all_accounts())
+
+    print(f'get_all_accounts result: {result.status_code}')
+    print(json.dumps(result.return_value, indent=4))
+
+
+if __name__ == '__main__':
+    main()
